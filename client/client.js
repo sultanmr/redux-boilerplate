d9eefcac8d01ddb56565545f1d3dd45b8f762d12
Sultan <sultanmr@hotmail.com>
2017-09-21 16:44:04 +0200

    using promise with thunk

--- client/client.js
+++ client/client.js
@@ -1,6 +1,7 @@
 import {applyMiddleware, createStore} from 'redux';
 import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
+import promise from 'redux-promise-middleware';
 import axios from "axios";
 
 const initialState = {
@@ -12,31 +13,36 @@ const initialState = {
 
 const reducer = (state=initialState, action) => {
     switch (action.type) {
-      case "FETCH_USERS_START":
+      case "FETCH_USERS_PENDING":
       return Object.assign({}, state, {fetching:true});
       break;
-      case "RECEIVE_USERS":
-      return Object.assign({}, state,  {fetching:false, fetched:true, users:action.info});
+      case "FETCH_USERS_FULFILLED":
+      return Object.assign({}, state,  {fetching:false, fetched:true, users:action.payload});
       break;
-      case "FETCH_USER_ERROR":
-      return Object.assign({}, state,  {fetching:false, error:action.info});
+      case "FETCH_USERS_REJECTED":
+      return Object.assign({}, state,  {fetching:false, error:action.payload});
       break;
     }
     return state;
 }
 
-const middleware = applyMiddleware(thunk,createLogger());
+const middleware = applyMiddleware(promise(), thunk, createLogger());
 
 const store = createStore (reducer,  middleware);
 
-store.dispatch((dispatch)=>
-{
-  dispatch ({type:"FETCH_USERS_START"});
-  axios.get("http://rest.learncode.academy/api/wstern/users")
-  .then((response)=> {
-    dispatch({type:"RECEIVE_USERS", info: response.data});
-  })
-  .catch((err)=> {
-    dispatch({type:"FETCH_USER_ERROR", info: err});
-  });
+store.dispatch ({
+  type: "FETCH_USERS",
+  payload: axios.get("http://rest.learncode.academy/api/wstern/users")
 });
+
+// store.dispatch((dispatch)=>
+// {
+//   dispatch ({type:"FETCH_USERS_START"});
+//   axios.get("http://rest.learncode.academy/api/wstern/users")
+//   .then((response)=> {
+//     dispatch({type:"RECEIVE_USERS", info: response.data});
+//   })
+//   .catch((err)=> {
+//     dispatch({type:"FETCH_USER_ERROR", info: err});
+//   });
+// });
