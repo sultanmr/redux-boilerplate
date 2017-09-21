4e298ee047a465bc527b3b267f3040edee7c6307
Sultan <sultanmr@hotmail.com>
2017-09-21 15:01:15 +0200

    redux-logger

--- client/client.js
+++ client/client.js
@@ -1,46 +1,16 @@
 import {applyMiddleware, createStore} from 'redux';
+import {createLogger} from 'redux-logger';
 
-const reducer = (state=0, action) => {
-  switch (action.type) {
-    case "INC":
-    return state+1;
-    break;
-    case "DEC":
-    return state-1;
-    break;
-    case "E":
-    throw new Error ("producing error from reducer");
-    default:
+const reducer = (state={}, action) => {
     return state;
-  }
-  
 }
 
-const logger = (store) => (next) => (action) => {
-  console.log ("inside logger", action);
-  next(action);
-}
+const middleware = applyMiddleware(createLogger());
 
-const error = (store) => (next) => (action) => {
-  console.log ("inside error", action);
-  try {
-    next (action);
-  } catch (e) {
-    console.log ("I HAVE CAUGHT AN ERROR...", e);
-  }
-}
+const store = createStore (reducer,  middleware);
 
-const middleware = applyMiddleware(logger, error);
+// store.subscribe (()=> {
+//   console.log ("store changed", store.getState());
+// });
 
-const store = createStore (reducer, 1, middleware);
-
-store.subscribe (()=> {
-  console.log ("store changed", store.getState());
-});
-
-store.dispatch({type:"INC"});
-store.dispatch({type:"INC"});
-store.dispatch({type:"INC"});
-
-store.dispatch({type:"DEC"});
-store.dispatch({type:"E"});
+store.dispatch({type:"FOO"});
