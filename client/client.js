a8cbfa85958f97f18b80c72e2bb2d47dba1347c0
Sultan <sultanmr@hotmail.com>
2017-09-21 15:08:41 +0200

    redux async

--- client/client.js
+++ client/client.js
@@ -1,11 +1,12 @@
 import {applyMiddleware, createStore} from 'redux';
 import {createLogger} from 'redux-logger';
+import thunk from 'redux-thunk';
 
 const reducer = (state={}, action) => {
     return state;
 }
 
-const middleware = applyMiddleware(createLogger());
+const middleware = applyMiddleware(thunk,createLogger());
 
 const store = createStore (reducer,  middleware);
 
@@ -13,4 +14,9 @@ const store = createStore (reducer,  middleware);
 //   console.log ("store changed", store.getState());
 // });
 
-store.dispatch({type:"FOO"});
+store.dispatch((dispatch)=>
+{
+  dispatch ({type:"FOO"});
+  //do something async
+  dispatch ({type:"FAA"});
+});
