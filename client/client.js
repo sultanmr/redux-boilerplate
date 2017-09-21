8f37856939d6fd0ef69b89b2febb9b7cdac14849
Sultan <sultanmr@hotmail.com>
2017-09-21 14:51:01 +0200

    applyMiddleware

--- client/client.js
+++ client/client.js
@@ -1,34 +1,45 @@
-import {combineReducers, createStore} from 'redux';
+import {applyMiddleware, createStore} from 'redux';
 
-const userReducer = (state={}, action) => {
-  switch(action.type) {
-    case "CHANGE_NAME": {
-      state = Object.assign({}, state, {name: action.info});
-      break;
-    }
-    case "CHANGE_AGE": {
-      state = Object.assign({}, state, {age: action.info});
-    }
+const reducer = (state=0, action) => {
+  switch (action.type) {
+    case "INC":
+    return state+1;
+    break;
+    case "DEC":
+    return state-1;
+    break;
+    case "E":
+    throw new Error ("producing error from reducer");
+    default:
+    return state;
   }
-  return state;
-};
+}
 
-const tweetsReducer = (state=[], action) => {
-  return state;
-};
+const logger = (store) => (next) => (action) => {
+  console.log ("inside logger", action);
+  next(action);
+}
 
-const reducers = combineReducers ({
-  user: userReducer,
-  tweets: tweetsReducer
-});
+const error = (store) => (next) => (action) => {
+  console.log ("inside error", action);
+  try {
+    next (action);
+  } catch (e) {
+    console.log ("I HAVE CAUGHT AN ERROR...", e);
+  }
+}
 
+const middleware = applyMiddleware(logger, error);
 
-const store = createStore(reducers);
+const store = createStore (reducer, 1, middleware);
 
-store.subscribe(()=> {
+store.subscribe (()=> {
   console.log ("store changed", store.getState());
 });
 
+store.dispatch({type:"INC"});
+store.dispatch({type:"INC"});
+store.dispatch({type:"INC"});
 
-store.dispatch({type:"CHANGE_NAME", info:'will'});
-store.dispatch({type:"CHANGE_AGE", info:35});
+store.dispatch({type:"DEC"});
+store.dispatch({type:"E"});
