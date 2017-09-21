3cbdc1a5eed04370fca2a53cf46112a84e3c6107
Sultan <sultanmr@hotmail.com>
2017-09-21 15:24:22 +0200

    async with axios

--- client/client.js
+++ client/client.js
@@ -1,8 +1,27 @@
 import {applyMiddleware, createStore} from 'redux';
 import {createLogger} from 'redux-logger';
 import thunk from 'redux-thunk';
+import axios from "axios";
 
-const reducer = (state={}, action) => {
+const initialState = {
+  fetching: false,
+  fetched:false,
+  users: [],
+  error: null
+};
+
+const reducer = (state=initialState, action) => {
+    switch (action.type) {
+      case "FETCH_USERS_START":
+      return Object.assign({}, state, {fetching:true});
+      break;
+      case "RECEIVE_USERS":
+      return Object.assign({}, state,  {fetching:false, fetched:true, users:action.info});
+      break;
+      case "FETCH_USER_ERROR":
+      return Object.assign({}, state,  {fetching:false, error:action.info});
+      break;
+    }
     return state;
 }
 
@@ -10,13 +29,14 @@ const middleware = applyMiddleware(thunk,createLogger());
 
 const store = createStore (reducer,  middleware);
 
-// store.subscribe (()=> {
-//   console.log ("store changed", store.getState());
-// });
-
 store.dispatch((dispatch)=>
 {
-  dispatch ({type:"FOO"});
-  //do something async
-  dispatch ({type:"FAA"});
+  dispatch ({type:"FETCH_USERS_START"});
+  axios.get("http://rest.learncode.academy/api/wstern/users")
+  .then((response)=> {
+    dispatch({type:"RECEIVE_USERS", info: response.data});
+  })
+  .catch((err)=> {
+    dispatch({type:"FETCH_USER_ERROR", info: err});
+  });
 });
