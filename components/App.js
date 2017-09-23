2011574c3c55e1914777456eb451cc61735f3309
Sultan <sultanmr@hotmail.com>
2017-09-23 15:56:10 +0200

    resolve fetchtweets error

--- components/App.js
+++ components/App.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import {connect} from 'react-redux';
 
-import * as user from '../api/redux/actions/user';
-
+import * as users from '../api/redux/actions/user';
+import * as tweets from '../api/redux/actions/tweets'
 
 class App extends React.Component {
 
         componentWillMount (props) {
-                this.props.dispatch(user.fetchUser());
+                this.props.dispatch(users.fetchUser());
+                this.fetchTweets();
+        }       
+
+        fetchTweets () {
+                this.props.dispatch(tweets.fetchTweets());
         }
         
        render () {
 
-        console.log(this.props);
+        const user = this.props.user.user;
+        const tweets = this.props.tweets;
+        if (!tweets.length) {
+                return <button onClick={this.fetchTweets.bind(this)}>load tweets</button>
+        }
+        const mappedTweets = tweets.map(tweet=><li>tweet.text</li>);
         return (<div>
-        
+        <p>{user.name}</p>
+        <ul>{mappedTweets}</ul>
         </div>);
         }
 }
